3a10cfcf2de0c9e660148b434c4d53f2362ba767
NathanJC2 <nathan_catapang@dlsu.edu.ph>
2026-07-07 23:38:13 +0800

    update repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+
 import react from '@astrojs/react';
-import icon from 'astro-icon';
 
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
-  base: 'virtual-exhibit-template',
+  integrations: [react(), mdx()]
 });
-
